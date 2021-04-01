bb3097f9263a27855c1a293a02ddf8446a0f8af8
5Roman5 <roman13101977@gmail.com>
2021-04-01 17:05:34 +0300

    Add resources and code

--- app/src/main/java/com/example/lesson3/BlankFragment.kt
+++ app/src/main/java/com/example/lesson3/BlankFragment.kt
@@ -1,10 +1,19 @@
 package com.example.lesson3
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.quiz_blank.*
+import java.io.IOException
+import java.io.InputStream
+import java.nio.channels.ServerSocketChannel.open
+
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -18,6 +27,7 @@ class BlankFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -31,6 +41,12 @@ class BlankFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.quiz_blank, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+
 
     companion object {
         /**
