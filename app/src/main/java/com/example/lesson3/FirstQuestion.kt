ceb9bdc81a5a7dfcf9e71d44e753314ece03834e
5Roman5 <roman13101977@gmail.com>
2021-04-03 15:26:50 +0300

    add button styles & listeners

--- app/src/main/java/com/example/lesson3/FirstQuestion.kt
+++ app/src/main/java/com/example/lesson3/FirstQuestion.kt
@@ -1,10 +1,15 @@
 package com.example.lesson3
 
+import android.annotation.SuppressLint
+import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.fragment_first_question.*
@@ -30,17 +35,24 @@ class FirstQuestion : Fragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
-            button.setOnClickListener(){
 
-            }
         }
     }
 
+    @SuppressLint("ResourceType")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         imageViewQuestion1.setImageResource(R.drawable.question1)
+        button1question1.setOnClickListener() {
+            seeRightAnswer()
+        }
+        button2question1.setOnClickListener() {
+            seeRightAnswer()
+        }
+        button3question1.setOnClickListener() {
+            seeRightAnswer()
+        }
     }
-
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -68,4 +80,12 @@ class FirstQuestion : Fragment() {
                     }
                 }
     }
+
+    private fun seeRightAnswer() {
+        button1question1.background.setColorFilter(ContextCompat.getColor(this.requireActivity(), R.color.red), PorterDuff.Mode.MULTIPLY);
+        button2question1.background.setColorFilter(ContextCompat.getColor(this.requireActivity(), R.color.green), PorterDuff.Mode.MULTIPLY);
+        button3question1.background.setColorFilter(ContextCompat.getColor(this.requireActivity(), R.color.red), PorterDuff.Mode.MULTIPLY);
+    }
 }
+
+
